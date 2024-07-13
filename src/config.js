db4d9b2f8e2659139642141931afcca977408023
DevTruce <DevTruce@gmail.com>
2024-07-13 18:22:34 -0400

    added api url/key in config

--- src/config.js
+++ src/config.js
@@ -7,9 +7,14 @@ require("dotenv").config();
 ///////////////////////////////////////////////////////////////////////////////
 //////// Config File Information Pulled from .env File
 const config = {
+  OWNER_ID: process.env.OWNER_ID || "",
+
   BOT_TOKEN: process.env.BOT_TOKEN || "",
   BOT_APP_ID: process.env.BOT_APP_ID || "",
   GUILD_ID: process.env.GUILD_ID || "",
+
+  NINJAS_API_URL: process.env.NINJAS_API_URL || "",
+  NINJAS_API_KEY: process.env.NINJAS_API_KEY || "",
 };
 
 ///////////////////////////////////////////////////////////////////////////////
