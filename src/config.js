5dcef7cc8715bdc04e2d8b6fa4083f1db618a5b9
DevTruce <DevTruce@gmail.com>
2024-07-14 14:38:28 -0400

    added rss feed config structure
    2 obj created for testing, adjust when we implement this fully

--- src/config.js
+++ src/config.js
@@ -12,10 +12,22 @@ const config = {
   BOT_TOKEN: process.env.BOT_TOKEN || "",
   BOT_APP_ID: process.env.BOT_APP_ID || "",
   GUILD_ID: process.env.GUILD_ID || "",
+
   TIMEOUT_INTERVAL: process.env.TIMEOUT_INTERVAL || 5000,
 
   NINJAS_API_URL: process.env.NINJAS_API_URL || "",
   NINJAS_API_KEY: process.env.NINJAS_API_KEY || "",
+
+  RSS_FEEDS: [
+    {
+      RSS_URL: process.env.RSS_REDDIT_PROGRAMMING_URL,
+      CHANNEL_ID: process.env.RSS_REDDIT_PROGRAMMING_CHANNEL_ID,
+    },
+    {
+      RSS_URL: process.env.RSS_CSS_TRICKS_URL,
+      CHANNEL_ID: process.env.RSS_CSS_TRICKS_CHANNEL_ID,
+    },
+  ],
 };
 
 ///////////////////////////////////////////////////////////////////////////////
