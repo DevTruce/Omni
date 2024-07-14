908716300501b8e6e92c3ca1213016a3c4300342
DevTruce <DevTruce@gmail.com>
2024-07-13 21:13:17 -0400

    added logs for msg /img sent

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -3,6 +3,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 //////// Imports/Variables
 const config = require("../config");
+const logger = require("../utils/logger");
 
 ///////////////////////////////////////////////////////////////////////////////
 //////// Event Listener for Message Creation
@@ -13,8 +14,18 @@ module.exports = {
   async execute(message) {
     if (message.author.bot) return; // ignore bot messages
 
+    if (!message.content) {
+      logger(
+        "discordAction",
+        `${message.author.tag} sent an image in ${message.channel.name}`
+      );
+    }
+
     if (message.content) {
-      console.log("test working, message received");
+      logger(
+        "discordAction",
+        `${message.author.tag} said ${message.content} in ${message.channel.name}`
+      );
     }
   },
 };
