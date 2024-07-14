500879fbcac629cf724950a51555b08aa8fae76a
DevTruce <DevTruce@gmail.com>
2024-07-14 19:45:18 -0400

    created rss feed feature

--- src/events/clientReady.js
+++ src/events/clientReady.js
@@ -3,7 +3,10 @@
 ///////////////////////////////////////////////////////////////////////////////
 //////// Imports & Variables
 const { Events, PresenceUpdateStatus, ActivityType } = require("discord.js");
-const logger = require("../utils/logger");
+const logger = require("../utils/logger.js");
+const checkRSSFeed = require("../utils/checkRSSFeed.js");
+
+const config = require("../config.js");
 
 ///////////////////////////////////////////////////////////////////////////////
 //////// Event Listener for Message Creation
@@ -11,6 +14,7 @@ module.exports = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
+    //// Setting Omni's initial values
     try {
       await client.user.setUsername("Omni");
       await client.user.setActivity("You", { type: ActivityType.Watching });
@@ -19,5 +23,15 @@ module.exports = {
       logger("error", "Failed to set bot status", "", err);
     }
     logger("info", `${client.user.tag} bot is online!`, "", "");
+
+    //// Handling RSS Feeds
+    // Check RSS Feed when the bot goes online
+    await checkRSSFeed(client);
+
+    // Check the RSS Feed every 5 minutes
+    setInterval(
+      async () => await checkRSSFeed(client),
+      config.RSS_TIMEOUT_INTERVAL
+    );
   },
 };
