63ce9c1d4e4e0b9cfbfe6c686b308accb6363d0a
DevTruce <DevTruce@gmail.com>
2024-07-13 20:33:24 -0400

    added timeout interval to config for ephmeralResponse

--- src/config.js
+++ src/config.js
@@ -12,6 +12,7 @@ const config = {
   BOT_TOKEN: process.env.BOT_TOKEN || "",
   BOT_APP_ID: process.env.BOT_APP_ID || "",
   GUILD_ID: process.env.GUILD_ID || "",
+  TIMEOUT_INTERVAL: process.env.TIMEOUT_INTERVAL || 5000,
 
   NINJAS_API_URL: process.env.NINJAS_API_URL || "",
   NINJAS_API_KEY: process.env.NINJAS_API_KEY || "",
